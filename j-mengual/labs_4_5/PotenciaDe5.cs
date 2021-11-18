bc8e35497ab745804ab0e9ae4d8b8f20704fed18
Andres Roca P <afrokap@gmail.com>
2021-11-18 12:03:41 -0500

    Labs potencia 5 y Sumador 15

---
+++ j-mengual/labs_4_5/PotenciaDe5.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JaiderLabs_4_5
+{
+    class PotenciaDe5
+    {
+        private string reading = "";
+        private int inNum = 0;
+        private int count = 0;
+        private int sum = 0;
+        private int[] nums = new int[10];
+        
+        public void ejecutar()
+        {
+            while (true)
+            {
+                       
+                Console.WriteLine("\n\n---------------------------------------------------------\nLABORATORIO PASO 3 - Ej22 - Bienvenido a la potencia de 5");
+                Console.WriteLine("\n>>Ingrese 10 números enteros negativos: ");
+                count = 0;
+                while (count < 10)
+                {
+                    Console.WriteLine("\n>>Ingrese un número entero negativo ('s', para salir) : ");
+                    Console.Write("\n>> #" + (count + 1) + " : ");
+                    reading = Console.ReadLine();
+                    if (int.TryParse(reading, out inNum) && inNum < 0)
+                    {
+                        nums[count] = inNum;
+                        count++;
+                        continue;
+                    }
+                    if (reading == "s") break;
+                    Console.WriteLine("xx Ingrese un valor válido xx");
+                }
+                if (reading == "s") break;
+                Console.WriteLine("\n\n¡Finalizado!.\n\nLa lista de las 10 potencias de 5 es : ");
+                for(int i = 0 ; i < 10 ; i++)
+                {
+                    Console.WriteLine("#" + (i+1) + ") " + nums[i] + " --> " + nums[i] + "^5 = " + Math.Pow(nums[i], 5));
+                }
+                Console.WriteLine("\n\n...Presione cualquier tecla para continuar");
+                Console.ReadKey();
+            }
+            Console.WriteLine("\n\n----------Ha salido. Hasta pronto!----------");
+            Console.WriteLine("\n\n...Presione cualquier tecla para continuar.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ j-mengual/labs_4_5/Sumador15.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JaiderLabs_4_5
+{
+    class Sumador15
+    {
+        private string reading = "";
+        private int inNum = 0;
+        private int count = 0;
+        private int sum = 0;
+        
+        public void ejecutar()
+        {
+            while (true)
+            {
+            Console.WriteLine("\n\n--------------------------------------------------------\nLABORATORIO PASO 3 - Bienvenido al Sumador de 15 números");
+            Console.WriteLine("\n>>Ingrese 15 números entre 50 y 500: ");
+                count = 0;
+                sum = 0;
+                while (count < 15)
+                {
+                    Console.WriteLine("\n>>Ingrese un número entre 50 y 500 ('s', para salir) : ");
+                    Console.Write("\n>> #"+(count+1)+" : ");
+                    reading = Console.ReadLine();
+                    if (int.TryParse(reading, out inNum) && 50 < inNum && inNum < 500)
+                    {
+                        sum += inNum;
+                        count++;
+                        continue;
+                    }
+                    if (reading == "s") break;
+                    Console.WriteLine("xx Ingrese un valor válido xx");
+                }
+
+                if (reading == "s") break;
+                
+                Console.WriteLine("\n\n¡Finalizado!.\nR/ La suma de los 15 valores es : "+sum);
+                Console.WriteLine("\n\n...Presione cualquier tecla para continuar.");
+                Console.ReadKey();
+            }
+            Console.WriteLine("\n\n----------Ha salido. Hasta pronto!----------");
+            Console.WriteLine("\n\n...Presione cualquier tecla para continuar.");
+            Console.ReadKey();
+        }
+    }
+}
