aaade269495b82de3ad72b852f5c50626a3aa4e1
Andres Roca P <afrokap@gmail.com>
2021-11-21 19:37:19 -0500

    CalculadoraIMC | PromedioNotas

---
+++ j-mengual/labs_4_5/CalculadorIMC.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JaiderLabs_4_5
+{
+    class CalculadorIMC
+    {
+        private string readingAltura = "";
+        private string readingPeso = "";
+        private float inNumAltura = 0;
+        private float inNumPeso = 0;
+        private double IMC = 0;
+        
+        public void ejecutar()
+        {
+            
+            while (true)
+            {
+                Console.WriteLine("\n\n-----------------------------------------------------------\nLABORATORIO PASO 4 - Ej14 - Bienvenido al Calculador de IMC");
+                Console.WriteLine("\n>>Ingrese la información: ");
+                
+                Console.Write("\n>>Ingrese su estatura en metros ('s' para salir. Use la COMA decimal) : ");
+                readingAltura = Console.ReadLine();
+                if (readingAltura == "s") break;
+                Console.Write(">>Ingrese su peso en kilogramos ('s' para salir. Use la COMA decimal) : ");
+                readingPeso = Console.ReadLine();
+                if (readingPeso == "s") break;
+
+                if (float.TryParse(readingAltura, out inNumAltura) && float.TryParse(readingPeso, out inNumPeso) 
+                    && 0.3 <= inNumAltura && inNumAltura <= 2.5 && 2 < inNumPeso && inNumPeso < 200)
+                {
+                    IMC = inNumPeso / Math.Pow(inNumAltura, 2);
+                }
+                else
+                {
+                    Console.WriteLine("readingAltura : " + readingAltura);
+                    Console.WriteLine("readingPeso : " + readingPeso); 
+                    Console.WriteLine("inNumAltura : "+inNumAltura);
+                    Console.WriteLine("inNumPeso : " + inNumPeso);
+                    Console.WriteLine("xx Ingrese un valor válido xx");
+                    continue;
+                }
+
+                Console.Write("\n\n¡Finalizado!.\n\nSu IMC es : " + IMC);
+                if (30 < IMC) Console.WriteLine(" -> Obesidad");
+                if (25 <= IMC && IMC < 29.9) Console.WriteLine(" -> Peso superior al normal");
+                if (18.5 <= IMC && IMC < 24.9) Console.WriteLine(" -> Normal");
+                if (IMC < 18.5) Console.WriteLine(" -> Peso Inferior al Normal");
+                
+                Console.WriteLine("\n\n...Presione cualquier tecla para continuar");
+                Console.ReadKey();
+            }
+            Console.WriteLine("\n\n----------Ha salido. Hasta pronto!----------");
+            Console.WriteLine("\n\n...Presione cualquier tecla para continuar");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ j-mengual/labs_4_5/PromedioNotas.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JaiderLabs_4_5
+{
+    class PromedioNotas
+    {
+        private string reading = "";
+        private float inNum = 0;
+        private int count;
+        private float prom = 0;
+        
+        public void ejecutar()
+        {
+            while (true)
+            {
+                Console.WriteLine("\n\n------------------------------------------------------------\nLABORATORIO PASO 4 - Ej5 - Bienvenido al Promedio de 3 notas");
+                Console.WriteLine("\n>>Ingrese 3 valores de notas entre 0 y 5: ");
+                prom = 0;
+                count = 0;
+                while (count < 3)
+                {
+                    Console.WriteLine("\n>>Ingrese el valor de la nota ('s', para salir) : ");
+                    Console.Write("\n>> #" + (count + 1) + " : ");
+                    reading = Console.ReadLine();
+                    if (float.TryParse(reading, out inNum) && 0 <= inNum && inNum <= 5)
+                    {
+                        prom += inNum/3;
+                        count++;
+                        continue;
+                    }
+                    if (reading == "s") break;
+                    Console.WriteLine("xx Ingrese un valor válido xx");
+                }
+                if (reading == "s")
+                {
+                    break;
+                }
+                Console.Write("\n\n¡Finalizado!.  El promedio de los 3 valores es : "+prom);
+                if (4.6 <= prom) Console.WriteLine(" -> Excelente");
+                if (4.0 <= prom && prom <= 4.5) Console.WriteLine(" -> Muy Bueno");
+                if (3.5 <= prom && prom < 4.0) Console.WriteLine(" -> Bueno");
+                if (3.0 <= prom && prom < 3.5) Console.WriteLine(" -> Satisfactorio");
+                if (prom < 3.0) Console.WriteLine(" -> Deficiente");
+                Console.WriteLine("\n\n...Presione cualquier tecla para continuar");
+                Console.ReadKey();
+            }
+            Console.WriteLine("\n----------Ha salido. Hasta pronto!----------");
+            Console.WriteLine("\n\n...Presione cualquier tecla para continuar.");
+            Console.ReadKey();
+        }
+    }
+}
